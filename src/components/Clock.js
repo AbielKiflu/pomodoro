4e53a3dab85b90fb229d28179ab5f621f59c01dc
Abel <abiel.kiflu.weld@gmail.com>
2022-12-04 18:34:11 +0100

    moving the hands of the clock

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -3,11 +3,32 @@ import React, { useState, useEffect } from "react";
 import "./clock.css";
 
 const Clock = () => {
+  const [time, setTime] = useState(new Date());
+  const interval = useEffect(() => {
+    setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const hour = time.getHours() * 30;
+  const minute = time.getMinutes() * 6;
+  const second = time.getSeconds() * 6;
+
   return (
     <div className="clock">
-      <div className="hour"></div>
-      <div className="minute"></div>
-      <div className="second"></div>
+      <div
+        className="hour"
+        style={{ transform: "rotate(" + hour + "deg)" }}
+      ></div>
+      <div
+        className="minute"
+        style={{ transform: "rotate(" + minute + "deg)" }}
+      ></div>
+      <div
+        className="second"
+        style={{ transform: "rotate(" + second + "deg)" }}
+      ></div>
       <span className="twelve">12</span>
       <span className="six">6</span>
       <span className="three">3</span>
